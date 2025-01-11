ddf5bc93d99e6826f51e1187d797399bb89c794e
mnrdsnts <meinardsantoss@gmail.com>
2025-01-11 19:12:47 +0800

    Added the navigation bar of the suggested stories

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 import { IoIosArrowDropright } from "react-icons/io";
 
 export default function Home() {
   return (
     <div>
+      {/* Highlights */}
       <section>
         <div className="grid grid-cols-[3fr_1fr] gap-4">
           <div className="h-[100%]">
@@ -67,6 +76,38 @@ export default function Home() {
                   <IoIosArrowDropright className="ml-3 text-xl"/>
                 </Link>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Suggested Stories */}
+      <section className="m-4 mt-10">
+        <div>
+          <div className="">
+            <h1 className="text-3xl">Stories that might be for you.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+          </div>
+          <div className="flex mt-5 justify-between items-center">
+            <div className="space-x-5">
+              <button className="bg-[var(--button-selected)] px-7 py-2 font-semibold rounded-full">All</button>
+              <button className="px-7 py-2 rounded-full">Fun and Exciting</button>
+              <button className="px-7 py-2 rounded-full">Cooking</button>
+              <button className="px-7 py-2 rounded-full">Drawing</button>
+              <button className="px-7 py-2 rounded-full">Lifestyle</button>
+              <button className="px-7 py-2 rounded-full">Chat</button>
+              <button className="px-7 py-2 rounded-full">Games</button>
+            </div>
+            <div className="flex items-center">
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Sort by" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Newest</SelectItem>
+                  <SelectItem value="dark">Oldest</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
         </div>
