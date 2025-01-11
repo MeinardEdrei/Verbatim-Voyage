79cfdac28508179cc59072afbbcceeea03f247e8
mnrdsnts <meinardsantoss@gmail.com>
2025-01-11 18:35:17 +0800

    Added texts and styles on the highlights

--- app/page.js
+++ app/page.js
@@ -1,34 +1,73 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import { IoIosArrowDropright } from "react-icons/io";
 
 export default function Home() {
   return (
     <div>
       <section>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="h-[78%]">
-            <Image 
-              src="https://placehold.co/300x200"
-              alt="Placeholder"
-              width={300}
-              height={200}
-              className="w-full h-full object-cover rounded-2xl"
-            />
+        <div className="grid grid-cols-[3fr_1fr] gap-4">
+          <div className="h-[100%]">
+            <div className="relative">
+              <Image 
+                src="https://placehold.co/300x200"
+                alt="Placeholder"
+                width={300}
+                height={200}
+                className="w-full h-full object-cover rounded-2xl"
+              />
+              <div className="absolute z-10 bottom-20 left-10">
+                <div className="relative mb-5">
+                  <p className="">Hopeful</p>
+                  <h2 className="font-bold text-3xl">Lorem Ipsum Lorem Ipsum</h2>
+                </div>
+                <Link href="/" className="bg-[var(--background)] px-6 py-3 rounded-full inline-flex items-center">
+                  Read this story
+                  <IoIosArrowDropright className="ml-3 text-xl"/>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="grid grid-rows-2 gap-4 h-[78%]">
-            <Image 
-              src="https://placehold.co/300x200"
-              alt="Placeholder"
-              width={300} 
-              height={200}
-              className="w-full h-full object-cover rounded-2xl"
-            />
-            <Image 
-              src="https://placehold.co/300x200"
-              alt="Placeholder"
-              width={300} 
-              height={200}
-              className="w-full h-full object-cover rounded-2xl"
-            />
+          <div className="grid grid-rows-2 gap-4 h-[100%]">
+            <div className="relative">
+              <Image 
+                src="https://placehold.co/300x200"
+                alt="Placeholder"
+                width={300} 
+                height={200}
+                className="w-full h-full object-cover rounded-2xl"
+              />
+              <div className="absolute z-10 bottom-10 left-10">
+                <div className="relative mb-5">
+                  <p className="text-sm">Hopeful</p>
+                  <h2 className="font-bold text-xl">Lorem Ipsum Lorem Ipsum</h2>
+                </div>
+                <Link href="/" className="bg-[var(--background)] text-sm px-5 py-2 rounded-full inline-flex items-center">
+                  Read this story
+                  <IoIosArrowDropright className="ml-3 text-xl"/>
+                </Link>
+              </div>
+            </div>
+            <div className="relative">
+              <Image 
+                src="https://placehold.co/300x200"
+                alt="Placeholder"
+                width={300} 
+                height={200}
+                className="w-full h-full object-cover rounded-2xl"
+              />
+              <div className="absolute z-10 bottom-10 left-10">
+                <div className="relative mb-5">
+                  <p className="text-sm">Hopeful</p>
+                  <h2 className="font-bold text-xl">Lorem Ipsum Lorem Ipsum</h2>
+                </div>
+                <Link href="/" className="bg-[var(--background)] text-sm px-5 py-2 rounded-full inline-flex items-center">
+                  Read this story
+                  <IoIosArrowDropright className="ml-3 text-xl"/>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
