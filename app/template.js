4b1541c6cf9f220e7a04bcfe14258f9705756d28
mnrdsnts <meinardsantoss@gmail.com>
2025-01-11 17:47:10 +0800

    Added layout style on header

--- app/template.js
+++ app/template.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 export default function Template({ children }) {
   return (
     <div>
-      <main>
+      <main className="mb-5">
         <Header />
       </main>
       {children}
