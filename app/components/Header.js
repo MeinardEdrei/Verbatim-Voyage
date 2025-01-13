f9fcae3adb13afa519c32c91c6990ada64ce281c
mnrdsnts <meinardsantoss@gmail.com>
2025-01-13 15:37:03 +0800

    Added sign in modal frontend

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,8 +2,27 @@
 
 import Link from "next/link";
 import { PiPuzzlePiece } from "react-icons/pi";
+import SignInModal from "./SignInModal";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const [showSignInModal, setShowSignInModal] = useState(false);
+  const signInRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (signInRef.current && !signInRef.current.contains(event.target)) {
+        setShowSignInModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div>
       <section className="flex justify-between">
@@ -15,10 +34,11 @@ const Header = () => {
           <PiPuzzlePiece className="mr-2 text-2xl" />
           Write
         </Link>
-        <Link href="/" className="border border-black px-6 py-3 rounded-full">Sign in</Link>
+        <button onClick={() => setShowSignInModal(true)} className="border border-black px-6 py-3 rounded-full">Sign in</button>
         <Link href="/" className="text-white bg-[#313131] px-6 py-3 rounded-full">Get Started</Link>
        </div>
       </section>
+      {showSignInModal && <SignInModal signInRef={signInRef} />}
     </div>
   )
 }
