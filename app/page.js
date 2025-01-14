718c1470b8c64917e12db3ed2f0aa4ff1888567d
mnrdsnts <meinardsantoss@gmail.com>
2025-01-14 21:29:31 +0800

    Created a separate file for top stories

--- app/page.js
+++ app/page.js
@@ -26,6 +26,7 @@ import { useEffect, useRef, useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import CategorySlider from "./components/CategorySlider";
+import useTopStories from "./utils/TopStories";
 
 const stories = [
   {
@@ -220,19 +221,9 @@ export default function Home() {
   const indexOfLastStory = currentPage * storiesPerPage;
   const indexOfFirstStory = indexOfLastStory - storiesPerPage;
   const [currentStories, setCurrentStories] = useState(stories.slice(indexOfFirstStory, indexOfLastStory));
-  const [topStories, setTopStories] = useState([]);
+  const topStories = useTopStories(stories, 3);
 
   const [totalPages, setTotalPages] = useState(Math.ceil(stories.length / storiesPerPage));
-
-  // Top stories
-  useEffect(() => {
-    const sortedStories = [...stories].sort((a, b) => {
-      const aStory = a.likesCount + a.commentsCount;
-      const bStory = b.likesCount + b.commentsCount;
-      return bStory - aStory;
-    });
-    setTopStories(sortedStories.slice(0, 3));
-  }, [stories]);
 
   // Stories category filter
   useEffect(() => {
---
+++ app/utils/TopStories.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+const useTopStories = (stories, topCount = 3) => {
+  const [topStories, setTopStories] = useState([]);
+
+  useEffect(() => {
+    const sortedStories = [...stories].sort((a, b) => {
+      const aStory = a.likesCount + a.commentsCount;
+      const bStory = b.likesCount + b.commentsCount;
+      return bStory - aStory;
+    });
+    setTopStories(sortedStories.slice(0, topCount));
+  }, [stories, topCount]);
+
+  return topStories;
+};
+
+export default useTopStories
