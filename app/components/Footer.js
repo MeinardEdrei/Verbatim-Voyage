403fccf5467b7329eba4020e18cb6483b028b316
mnrdsnts <meinardsantoss@gmail.com>
2025-01-15 17:35:28 +0800

    Added mobile responsiveness in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,14 +6,14 @@ const Footer = () => {
     <div className="m-4">
       <div className="flex flex-col border border-black/30 rounded-2xl px-10 py-5">
         <div className="flex justify-between">
-          <h1 className="text-3xl">Verbatim Voyage</h1>
-          <Link href="/" className="border border-black p-4 rounded-full flex items-center">
+          <h1 className="text-sm xl:text-3xl">Verbatim Voyage</h1>
+          <Link href="/" className="hidden xl:flex border border-black p-4 rounded-full items-center">
             Made by Meinard Edrei S.&nbsp;
             <MdArrowOutward className="text-2xl"/>
           </Link>
         </div>
-        <div>
-          <p className="text-[#747474] font-light">@2025 Verbatim Voyage. All rights reserved.</p>
+        <div className="mt-5">
+          <p className="text-xs xl:text-base text-[#747474] font-light">@2025 Verbatim Voyage. All rights reserved.</p>
         </div>
       </div>
     </div>
