5d9c158622a7fe54272edebd4da2e5b3cee72bfb
mnrdsnts <meinardsantoss@gmail.com>
2025-01-16 16:39:15 +0800

    Added user helper for auth

---
+++ utils/userHelper.js
@@ -0,0 +1,19 @@
+import dbConnect from "@/lib/db";
+import User from "@/models/User";
+
+export async function FindOrCreateUser({ name, email, image, provider, providerId }) {
+  await dbConnect();
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    user = await User.create({
+      name,
+      email,
+      image,
+      provider,
+      providerId,
+    });
+  }
+
+  return user;
+}
