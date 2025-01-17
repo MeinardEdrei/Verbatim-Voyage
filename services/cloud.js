a9196fbae04a3af37933ff1b7049016665fe9e0c
mnrdsnts <meinardsantoss@gmail.com>
2025-01-17 16:53:42 +0800

    ➕ Added image upload service

--- services/cloud.js
+++ services/cloud.js
@@ -1,3 +1,4 @@
+import { useUserSession } from "@/app/utils/SessionContext";
 import axios from "axios";
 
 export const createSignature = async ({ transformation }) => {
@@ -14,5 +15,32 @@ export const createSignature = async ({ transformation }) => {
   } catch (error) {
     console.error("Signature generation error: ", error);
     throw new error(error.response?.data?.message || "Failed to generate signature");
+  }
+}
+
+export const uploadImage = async ({ file }) => {
+  try {
+    const session = useUserSession();
+    const public_id = `verbatim_voyage/users/${session?.userSession?.user?.name}/blog_images/image_${Date.now()}`;
+    const transformation = { crop: 'fill' };
+    
+    const {signature, timestamp } = await createSignature({ public_id, transformation });
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY);
+    formData.append("signature", signature);
+    formData.append("timestamp", timestamp);
+    formData.append("public_id", public_id);
+
+    const response = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+      formData
+    );
+
+    const data = response.data;
+
+    return data.secure_url;
+  } catch (error) {
+    throw new error(error.response?.data?.message || "Failed to upload image");
   }
 }
