92b7f63696fe7c86eaa8f75f0bc9f39944475a32
mnrdsnts <meinardsantoss@gmail.com>
2025-01-19 13:46:18 +0800

    🔌 Initialized fetching data

--- app/[user]/[post]/page.js
+++ app/[user]/[post]/page.js
@@ -1,8 +1,25 @@
 "use client"
+import { fetchThisStory } from "@/services/stories";
 import { useParams } from "next/navigation"
+import { useEffect, useState } from "react";
 
 const page = () => {
   const { user, post } = useParams();
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await fetchThisStory({ user, post });
+
+      if (response.status === 200) {
+        setStory(response);
+      } else {
+        console.error(response.message);
+      }
+    }
+
+    fetch();
+  }, []);
 
   return (
     <div>
