29bb21917f6a60662684f2b57ed2c63e35797159
mnrdsnts <meinardsantoss@gmail.com>
2025-01-21 11:31:03 +0800

    🛰️ Created like API

---
+++ app/api/story/like/route.js
@@ -0,0 +1,19 @@
+import dbConnect from "@/lib/db";
+import User from "@/models/User";
+
+export async function GET(req) {
+  await dbConnect()
+
+  try {
+    const storyId = req.nextUrl.searchParams.get('storyId');
+    const userId = req.nextUrl.searchParams.get('userId');
+
+    const isLiked = await User.findOne({ _id: userId, likedStories: storyId });
+
+    return !!isLiked; // converted to boolean by using (!!)
+  } catch (error) {
+    console.log('User likes api error:', error);
+    return new Response(JSON.stringify({ message: "An unexpected error occured." }),
+    { status: 500 });
+  }
+}
