92b1a27674a00bf6ccced3c021dd2211094ac90b
mnrdsnts <meinardsantoss@gmail.com>
2025-01-21 13:43:19 +0800

    🛰️ Created post endpoint for like

--- app/api/story/like/route.js
+++ app/api/story/like/route.js
@@ -1,4 +1,5 @@
 import dbConnect from "@/lib/db";
+import Story from "@/models/Story";
 import User from "@/models/User";
 
 export async function GET(req) {
@@ -16,5 +17,32 @@ export async function GET(req) {
     console.log('User likes api error:', error);
     return new Response(JSON.stringify({ message: "An unexpected error occured." }),
     { status: 500 });
+  }
+}
+
+export async function POST(req) {
+  await dbConnect();
+
+  try {
+    const { storyId, userId } = await req.json();
+    const user = await User.findById(userId);
+    const story = await Story.findById(storyId);
+
+    if (!user.likedStories.includes(storyId)) {
+      user.likedStories.push(storyId);
+      story.likes += 1;
+      await Promise.all([ user.save(), story.save()]);
+    } else {
+      user.likedStories.pull(storyId);
+      story.likes -= 1;
+      await Promise.all([ user.save(), story.save()]);
+    }
+    
+    return new Response(JSON.stringify({ message: 'Like success'}),
+    { status: 200 });
+  } catch (error) {
+    console.log('User likes api error:', error);
+    return new Response(JSON.stringify({ message: "An unexpected error occured. " }),
+  { status: 500 });
   }
 }
