5a115862397d878fc45511339ed9de3b7bf61200
mnrdsnts <meinardsantoss@gmail.com>
2025-01-21 11:16:34 +0800

    ✨ Created user likes service

--- services/stories.js
+++ services/stories.js
@@ -33,6 +33,26 @@ export const fetchThisStory = async (data) => {
   }
 }
 
+export const isStoryLiked = async (data) => {
+  try {
+    const response = await axios.get(`/api/story/like?storyId=${data}`,
+      {
+        headers: {
+          'Content-Type' : 'application/json',
+        }
+      }
+    )
+
+    return {
+      status: 200,
+      data: response.data,
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error(error.response?.data?.message || "Failed to fetch 'isStoryLiked'")
+  }
+}
+
 export const createStory = async (data) => {
   try {
     const response = await axios.post('api/story', data, {
