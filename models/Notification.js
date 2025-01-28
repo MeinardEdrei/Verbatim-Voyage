e929e5d76b3ae01ac82cea067e12b20d9d8c91a9
mnrdsnts <meinardsantoss@gmail.com>
2025-01-28 13:48:24 +0800

    ➕ Added two new notification schema fields

--- models/Notification.js
+++ models/Notification.js
@@ -4,6 +4,9 @@ const { Schema } = mongoose;
 const notificationSchema = new Schema({
   type: { type: String, required: true },
   action: { type: String, required: true },
+  target: { type: Schema.Types.ObjectId, refPath: 'targetModel' },
+  targetModel: { type: String, enum: ['Story', 'User'] },
+  recipient: { type: Schema.Types.ObjectId, ref: 'User' },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
 });
