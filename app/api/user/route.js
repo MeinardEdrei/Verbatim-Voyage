db1049cbf89d6e174735c78c97858e3040abfb13
mnrdsnts <meinardsantoss@gmail.com>
2025-01-30 14:17:14 +0800

    🛰️ Created user API

---
+++ app/api/user/route.js
@@ -0,0 +1,22 @@
+import dbConnect from "@/lib/db";
+import User from "@/models/User";
+
+export async function GET(req) {
+  await dbConnect();
+
+  try {
+    const userId = req.nextUrl.searchParams('userId');
+    const user = await User.findById(userId)
+      .select('likedStories')
+      .lean();
+
+    if (!user) return new Response('User not found', { status: 404 });
+
+    return new Response(JSON.stringify(user),
+    { status: 200, headers: { "Content-Type": "application/json" } });
+  } catch (error) {
+    console.log('Fetching user API error:', error);
+    return new Response(JSON.stringify({ message: 'An unexpected error occured.', error: error }),
+    { status: 500, headers: { 'Content-Type' : 'application/json' } } );
+  }
+}
