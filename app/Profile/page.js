f376a6d09cb17947b34ec22f38d2c8e53b179b1e
mnrdsnts <meinardsantoss@gmail.com>
2025-01-30 15:47:26 +0800

    🎨 Styled profile page

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -41,30 +41,46 @@ const page = () => {
   }, [popular]);
   
   return (
-    <div hidden={session.userSession ? false : true} className="m-4">
-      <section className="grid xl:grid-cols-[70%_30%] gap-10 w-full xl:w-[90%]">
-        <div className="flex gap-4">
+    <div hidden={session.userSession ? false : true} className="m-2 w-full h-screen">
+      <section className="grid xl:grid-cols-[70%_30%] gap-10 h-full w-full xl:w-[90%]">
+        <div className="max-w-full mt-20 xl:mx-20">
           {/* User Details */}
-          <div>
-            <div>
-              <Image 
-                src={session?.userSession?.user?.image || '/darklogo.svg'}
-                width={100}
-                height={100}
-                alt="User Profile"
-                className="rounded-full"
-              />
+          <div className="w-full h-[30vw] xl:h-[10vw] border-b-2 border-[var(--border-light)]">
+            <div className="flex w-full items-center gap-5">
+              <div>
+                <Image 
+                  src={session?.userSession?.user?.image || '/darklogo.svg'}
+                  width={120}
+                  height={120}
+                  alt="User Profile"
+                  className="rounded-full object-cover"
+                />
+              </div>
+              <div>
+                <h2 className="capitalize font-bold text-xl xl:text-2xl">{session?.userSession?.user?.name}</h2>
+                <div className="flex gap-2 text-xs xl:text-sm mt-2">
+                  <p>1 following</p>
+                  <p>2 followers</p>
+                </div>
+                <button className="bg-[var(--button-selected)] text-sm mt-4 text-white px-6 py-2 rounded-full">Edit Profile</button>
+              </div>
             </div>
             <div>
-              <h2 className="capitalize font-bold text-2xl">{session?.userSession?.user?.name}</h2>
             </div>
           </div>
           {/* Liked Stories */}
-          <div>
-
-          </div>
-          <div>
-
+          <div className="mt-14">
+            <div className="">
+              <h2 className="underline text-xs xl:text-sm mb-7">Liked Stories</h2>
+            </div>
+            { user?.likedStories?.length > 0 ? (
+              user.likedStories.map((item) => (
+                <div key={item._id}>
+                </div>
+              ))
+            ) : (
+              <p className="place-self-center text-xs">Read more stories so you can see your favorites here.</p>
+            )}
           </div>
         </div>
         <PopularReads 
