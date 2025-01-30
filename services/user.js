e0db504d386ffb8b35b44c999d31ac00ea5b5f25
mnrdsnts <meinardsantoss@gmail.com>
2025-01-30 17:25:09 +0800

    ✨ Created update profile service

--- services/user.js
+++ services/user.js
@@ -13,3 +13,23 @@ export const fetchUser = async (data) => {
     throw new Error(error.response?.data?.message || "Failed to fetch user");
   }
 }
+
+export const updateProfile = async (data) => {
+  try {
+    const response = await axios.post(`/api/user/profile?username=${data}`,
+      {
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      }
+    );
+
+    return {
+      status: 200,
+      data: response.data
+    }
+  } catch (error) {
+    console.log('Updating profile error:', error);
+    throw new Error(error.response?.data?.message || "Failed to update profile");
+  }
+}
