753a59cea151fc8a9118890bd1474dde5fe47d35
mnrdsnts <meinardsantoss@gmail.com>
2025-01-30 15:53:12 +0800

    📦 Installed shadcn input and label

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -8,6 +8,19 @@ import { useUserSession } from "../utils/SessionContext";
 import { fetchUser } from "@/services/user";
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 const page = () => {
   const [stories, setStories] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -62,7 +75,37 @@ const page = () => {
                   <p>1 following</p>
                   <p>2 followers</p>
                 </div>
-                <button className="bg-[var(--button-selected)] text-sm mt-4 text-white px-6 py-2 rounded-full">Edit Profile</button>
+                {/* <button className="bg-[var(--button-selected)] text-sm mt-4 text-white px-6 py-2 rounded-full">Edit Profile</button> */}
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">Edit Profile</Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Edit profile</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your profile here. Click save when you're done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                          Name
+                        </Label>
+                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                      </div>
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                          Username
+                        </Label>
+                        <Input id="username" value="@peduarte" className="col-span-3" />
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
             <div>
